82756d072bfc283a8afe31efe0282a1d726f0104
ObadiahCrowe <obadiahpcrowe@gmail.com>
2016-05-10 15:28:21 +0930

    pci fixes

--- kernel/drivers/pci/pci.asm
+++ kernel/drivers/pci/pci.asm
@@ -11,36 +11,12 @@ os_pci_read_reg:
 	and ebx, 0x00ffffff		; Clear bits 31 - 24
 	or ebx, 0x80000000		; Set bit 31
 	mov eax, ebx
-	mov dx, PCI_CONFIG_ADDRESS
+	mov dx, 0x0CF8
 	out dx, eax
-	mov dx, PCI_CONFIG_DATA
+	mov dx, 0x0CFC
 	in eax, dx
 
 	pop rbx
 	pop rcx
 	pop rdx
 ret
-
-
-;Configuration Mechanism One has two IO port rages associated with it.
-;The address port (0xcf8-0xcfb) and the data port (0xcfc-0xcff).
-;A configuration cycle consists of writing to the address port to specify which device and register you want to access and then reading or writing the data to the data port.
-
-PCI_CONFIG_ADDRESS	EQU	0x0CF8
-PCI_CONFIG_DATA		EQU	0x0CFC
-
-;ddress dd 10000000000000000000000000000000b
-;          /\     /\      /\   /\ /\    /\
-;        E    Res    Bus    Dev  F  Reg   0
-; Bits
-; 31		Enable bit = set to 1
-; 30 - 24	Reserved = set to 0
-; 23 - 16	Bus number = 256 options
-; 15 - 11	Device/Slot number = 32 options
-; 10 - 8	Function number = will leave at 0 (8 options)
-; 7 - 2		Register number = will leave at 0 (64 options) 64 x 4 bytes = 256 bytes worth of accessible registers
-; 1 - 0		Set to 0
-
-
-; =============================================================================
-; EOF
